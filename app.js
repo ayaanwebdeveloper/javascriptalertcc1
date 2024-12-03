61b80c9562dc9dd2d957121941d5def610ef61eb
AyaanWebDeveloper <developwithayaan@gmail.com>
2024-12-03 06:22:10 -0800

    alert of javascript

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Error! Please enter a valid password ")
+alert("Welcome to JS Land\nHappy Codeing")
+alert('Welcome To JS Land')
+alert("Happy Codeing\nPreventing this page additional dialog box")
+console.log("this my console my name is ayaan faheem")
